cc2cd62d32a3652f31a29acd1256fb0747ccca79
Igor Marjanovic <igor.marjanovic@shiftplanning.com>
2017-03-06 14:14:22 +0100

    -

---
+++ src/partials/home-page/section-one.php
@@ -0,0 +1,111 @@
+<!-- start first section -->
+<section class="first-section pY40">
+    <div class="container">
+        <div class="row top">
+
+            <div class="col-md-7 col-sm-7 col-xs-12 column">
+                <div class="flexslider">
+                    <ul class="slides">
+                        <?php
+                            //== get meta data for category
+                            $key = "Slider First Section";
+                            $slideKey = "Number of Slides in First Section";
+                            //== set category id
+                            $cat_id = get_post_meta($post->ID, $key, true);
+                            //== set slide number
+                            $slide_num = get_post_meta($post->ID, $slideKey, true);
+                            //== arguments
+                            $args = array(
+                                'posts_per_page' => $slide_num,
+                                'cat' => $cat_id
+                            );
+                            query_posts($args);
+                            while (have_posts()) : the_post();
+                        ?>
+                        <li>
+                            <a href="<?php the_permalink() ?>" class="thumbHolder">
+                                <span class="thumbShadow">
+                                    <span class="thumbCategory">
+                                        <?php echo get_cat_name( $cat_id ) ?>
+                                    </span>
+                                    <span class="thumbTitle">
+                                    <?php
+                                        the_title();
+                                    ?>
+                                    </span>
+                                </span>
+                                <img src="<?php the_post_thumbnail_url(); ?>" class="thumb">
+                            </a>
+                        </li>
+                        <?php 
+                            endwhile;
+                            wp_reset_query();
+                        ?>
+                    </ul>
+                </div>                    
+            </div>
+            <div class="col-md-5 col-sm-5 col-xs-12 column">
+                <?php
+                    //== set category id
+                    $cat_id_for_second = 2;
+                    //== arguments
+                    $args = array(
+                        'posts_per_page' => 1,
+                        'cat' => $cat_id_for_second
+                    );
+                    query_posts($args);
+                    while (have_posts()) : the_post();
+                ?>
+                <a href="<?php the_permalink() ?>" class="thumbHolder">
+                    <span class="thumbShadow">
+                        <span class="thumbCategory">
+                            <?php echo get_cat_name( $cat_id_for_second ) ?>
+                        </span>
+                        <span class="thumbTitle">
+                        <?php
+                            the_title();
+                        ?>
+                        </span>
+                    </span>
+                    <img src="<?php the_post_thumbnail_url(); ?>" class="thumb">
+                </a>
+                <?php
+                    endwhile;
+                    wp_reset_query();
+                ?>
+            </div>
+        </div><!-- . / end first row --> 
+        <!-- start second row -->
+        <div class="row bottom">
+            <?php
+                //== set category id
+                $cat_id_for_bottom = 22;
+                //== arguments
+                $args = array(
+                    'posts_per_page' => 4,
+                    'cat' => $cat_id_for_bottom,
+                    'offset'=> 2
+                );
+                query_posts($args);
+                while (have_posts()) : the_post();
+            ?>
+            <a href="<?php the_permalink() ?>" class="thumbHolder col-md-3 col-sm-3 col-xs-12 column">
+                <span class="thumbShadow">
+                    <span class="thumbCategory">
+                        <?php echo get_cat_name( $cat_id_for_bottom ) ?>
+                    </span>
+                    <span class="thumbTitle">
+                    <?php
+                        the_title();
+                    ?>
+                    </span>
+                </span>
+                <img src="<?php the_post_thumbnail_url(); ?>" class="thumb">
+            </a>
+            <?php
+                endwhile;
+                wp_reset_query();
+            ?>
+        </div>
+    </div>
+</section><!-- . / end first section -->
---
+++ src/partials/home-page/section-two.php
@@ -0,0 +1,58 @@
+<!-- start second section -->
+<section class="second-section">
+    <div class="container">
+        <div class="sectionTitle">
+            <span class="title">
+                Vesti iz nocnog zivota
+            </span>
+        </div>
+        <div class="row">
+            <?php
+                //== set category id
+                $cat_id = 4;
+                //== arguments
+                $args = array(
+                    'posts_per_page' => 3,
+                    'cat' => $cat_id,
+                    'offset'=> 8
+                );
+                query_posts($args);
+                while (have_posts()) : the_post();
+            ?>
+            <a href="<?php the_permalink() ?>" class="thumbHolder col-sm-4 column">
+                <span class="thumbCategory">
+                    <?php echo get_cat_name( $cat_id ) ?>
+                </span>
+                <img src="<?php the_post_thumbnail_url(); ?>" class="thumb">
+                <div class="cnt">
+                    <span class="thumbTitle">
+                        <?php
+                            the_title();
+                        ?>
+                    </span>
+                    <span class="getAuthor">
+                        <?php
+                            echo 'By ';
+                            echo '<span>' . get_the_author_link() . '</span>';
+                        ?>
+                    </span>
+                    <span class="postTime">
+                        <?php
+                            echo ' / ';
+                            echo get_post_time('F d, Y.');
+                        ?>
+                    </span>
+                    <p class="clearfix">
+                        <?php echo wp_trim_words( get_the_content(), 40, '...' ); ?>
+                    </p>
+                    <button href="<?php get_permalink(); ?>">READ MORE</button>
+
+                </div>
+            </a>
+            <?php
+                endwhile;
+                wp_reset_query();
+            ?>
+        </div>
+    </div>
+</section><!-- . / end second section -->
