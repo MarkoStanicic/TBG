b4ac8177b37def8c1cee959b972e9b43fc5ece45
Marko <marko.b.stanicic@gmail.com>
2017-05-09 23:35:24 +0200

    .

--- src/category.php
+++ src/category.php
@@ -3,17 +3,21 @@
 	<main role="main" aria-label="Content">
 		<!-- section -->
 		<section>
+			<div class="container">
+                <div class="col-sm-9">
+                    <h1><?php _e( 'Category: ', 'html5blank' ); single_cat_title(); ?></h1>
 
-			<h1><?php _e( 'Category: ', 'html5blank' ); single_cat_title(); ?></h1>
+                    <?php get_template_part('loop'); ?>
 
-			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
+                    <?php get_template_part('pagination'); ?>
+                </div>
+                <div class="col-sm-3">
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
 
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
